35651caae05c348a1759ce2fb2985e56f8876bb1
Attila Jamilov <attila.jamilov@gmail.com>
2025-06-23 01:33:46 -0700

    updates

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -15,3 +15,9 @@ vim.keymap.set("n", "<up>", "gk")
 
 vim.keymap.set("n", "<C-s>", ":w<CR>")
 vim.keymap.set("i", "<C-s>", ":w<CR>")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
